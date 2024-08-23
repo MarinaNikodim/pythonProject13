20fce4d3d665ea2b1ae3d8161f87a867fa3e87e6
mnikodim <Marinenik@yandex.ru>
2024-08-23 16:36:33 +0300

    I hope <correct>

---
+++ module_2_3.py
@@ -0,0 +1,47 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+new_list=[item for item in my_list if item != 0]
+while index < len(new_list):
+    if new_list[index] < 0:
+        break
+    else:
+        print(new_list[index])
+        index += 1
+
+#2nd variation
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+my_list.remove(0)
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    else:
+        print(my_list[index])
+        index += 1
+
+#while index < len(my_list):
+#    if my_list[index] == 0:
+#        continue
+#    if my_list[index] < 0:
+#        break
+#    else:
+#        print(my_list[index])
+#        index += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
