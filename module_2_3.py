5ed9f5b8ac44edc754bd5576c09adf36a64b791d
mnikodim <Marinenik@yandex.ru>
2024-08-23 16:52:16 +0300

    add question in 3d vertion

--- module_2_3.py
+++ module_2_3.py
@@ -6,9 +6,9 @@ while index < len(new_list):
         break
     else:
         print(new_list[index])
-        index += 1
+        index = index + 1
 
-#2nd variation
+#2nd vertion
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 my_list.remove(0)
@@ -17,8 +17,8 @@ while index < len(my_list):
         break
     else:
         print(my_list[index])
-        index += 1
-
+        index = index + 1
+#3d vertion Прошу пояснить, почему в этом варианте не работает программа, что не так?
 #while index < len(my_list):
 #    if my_list[index] == 0:
 #        continue
